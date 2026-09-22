bacb2033b830683c52f75ebc5701ad6d483954d6
vyombudha <vyombudha7@gmai.com>
2026-09-22 17:47:36 +0545

    feat: refactor header layout in HomePage and ProjectPage components; enhance responsiveness and search input styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,18 +15,18 @@ export default function HomePage() {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="flex h-full w-full flex-col rounded-3xl bg-background p-8">
-        <header className="flex flex-row items-center justify-between gap-16 p-8">
-          <Input
-            className="h-full max-w-1/3"
-            type="search"
-            placeholder="Search Projects..."
-          />
-          <div className="flex flex-row items-center justify-between gap-8">
+        <header className="flex flex-col items-center justify-between gap-16 p-8 md:flex-row-reverse lg:flex-row-reverse">
+          <div className="flex w-full flex-row items-center justify-between md:w-fit md:gap-8 lg:w-fit lg:gap-8">
             <Button onClick={() => setNewProjectMenuOpen(true)} size={"lg"}>
               <Plus /> New Project
             </Button>
             <UserButton />
           </div>
+          <Input
+            className="h-full w-full md:w-1/3 lg:w-1/3"
+            type="search"
+            placeholder="Search Projects..."
+          />
         </header>
 
         <main className="min:grid-cols-1 grid flex-1 scrollbar-none content-start gap-8 overflow-y-auto px-4 py-8 md:grid-cols-2 lg:grid-cols-4">
--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -27,18 +27,18 @@ function ProjectPage() {
       <SideBar open={sideBarOpen} onOpenChange={setSideBarOpen} />
       <div className="flex h-full w-full items-center justify-center">
         <div className="flex h-full w-full flex-col rounded-3xl bg-background p-8">
-          <header className="flex flex-row items-center justify-between gap-16 p-8">
-            <Input
-              className="h-full max-w-1/3"
-              type="search"
-              placeholder="Search Projects..."
-            />
-            <div className="flex flex-row items-center justify-between gap-8">
+          <header className="flex flex-col items-center justify-between gap-16 p-8 md:flex-row-reverse lg:flex-row-reverse">
+            <div className="flex w-full flex-row items-center justify-between md:w-fit md:gap-8 lg:w-fit lg:gap-8">
               <Button onClick={handleNewBtnClick()} size={"lg"}>
                 <Plus /> New {newEntityName}
               </Button>
               <UserButton />
             </div>
+            <Input
+              className="h-full w-full md:w-1/3 lg:w-1/3"
+              type="search"
+              placeholder="Search Journals/Notes..."
+            />
           </header>
           <Routes>
             <Route
